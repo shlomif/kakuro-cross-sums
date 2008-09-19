68a947552fec3308fa0e8b80eb1f4e5fb33cd449
shlomif <shlomif@b384bcd7-cfd4-0310-aca0-d78b80f7b91b>
2008-09-19 13:47:10 +0000

    Added more tests.

--- solver/ruby/t/check-perms.rb
+++ solver/ruby/t/check-perms.rb
@@ -1,5 +1,10 @@
 require "kakuro-perms.rb"
 
+def human_get_perms(mutator, sum, num)
+    perms = mutator.get_perms(*mutator.human_to_internal(sum, num))
+    return perms.map {|x| mutator.internal_to_human(x) }
+end
+
 describe "Kakuro" do
     before do
         @perm = Kakuro::Perms.new
@@ -35,4 +40,39 @@ describe "Kakuro" do
         (@perm.get_perms(*@perm.human_to_internal(10,4)).map {|x| @perm.internal_to_human(x) }).should eql([[1,2,3,4]])
     end
 
+    it "get_perms(10/4)" do
+        human_get_perms(@perm, 10, 4).should eql([[1,2,3,4]])
+    end
+
+    it "get_perms(3/2)" do
+        human_get_perms(@perm, 3, 2).should eql([[1,2]])
+    end
+
+    it "get_perms(16/2)" do
+        human_get_perms(@perm, 16, 2).should eql([[7,9]])
+    end
+
+    it "get_perms(7/3)" do
+        human_get_perms(@perm, 7, 3).should eql([[1,2,4]])
+    end
+
+    it "gen_perms(25/5)" do
+        human_get_perms(@perm, 25, 5).should eql([
+            [1,2,5,8,9],
+            [1,2,6,7,9],
+            [1,3,4,8,9],
+            [1,3,5,7,9],
+            [1,3,6,7,8],
+            [1,4,5,6,9],
+            [1,4,5,7,8],
+            [2,3,4,7,9],
+            [2,3,5,6,9],
+            [2,3,5,7,8],
+            [2,4,5,6,8],
+            [3,4,5,6,7],
+        ])
+    end
 end
+
+
+
