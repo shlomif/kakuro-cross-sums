b308b817e35f7bfaad43361d4e4c5e476f01e889
shlomif <shlomif@b384bcd7-cfd4-0310-aca0-d78b80f7b91b>
2010-05-26 17:07:49 +0000

    Add a more complex test that appears to pass.

--- solver/ruby/t/merge-constraints.rb
+++ solver/ruby/t/merge-constraints.rb
@@ -31,3 +31,29 @@ describe "Merge Two Constraints" do
     end
 end
 
+describe "[[1,2]] and [[1,8],[2,7],[3,6],[4,5]]" do
+    it "should be merged" do
+        vert = Kakuro::Constraint.new(
+            *Kakuro::Perms.new.human_to_internal(3,2)
+        )
+        horiz = Kakuro::Constraint.new(
+            *Kakuro::Perms.new.human_to_internal(9,2)
+        )
+
+        merger = Kakuro::CellConstraintsMerger.new(
+            {
+                'constraints' => [vert,horiz,],
+            }
+        )
+        
+        merger.remaining_dir_constraints(Kakuro::Vert).should == \
+            [(1 << 0)|(1 << 1)]
+        merger.remaining_dir_constraints(Kakuro::Horiz).should == \
+            [ ((1 << 0)|(1 << 7)), ((1 << 1)|(1 << 6)), ]
+
+        merger.possible_cell_values.should == ((1 << 0)|(1 << 1))
+
+        merger.has_single_verdict.should == false
+    end
+end
+
