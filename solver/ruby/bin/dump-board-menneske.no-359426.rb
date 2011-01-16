f049242546df625f60c35e870ba6ac77bb3b24a3
shlomif <shlomif@b384bcd7-cfd4-0310-aca0-d78b80f7b91b>
2011-01-16 10:53:31 +0000

    Add board No. 359426.

---
+++ solver/ruby/bin/dump-board-menneske.no-359426.rb
@@ -0,0 +1,37 @@
+require "kakuro-board.rb"
+
+puts "Before new"
+$BOARD = Kakuro::Board.new
+puts "After new"
+$BOARD.parse(<<'EOF')
+[\]     [17\]       [16\]       [7\]        [\]     [\]     [\]     [\]
+[\19]   []          []          []          [\]     [\]     [16\]   [3\]
+[\17]   []          []          []          [29\]   [4\11]  []      []
+[\]     [\]         [\23]       []          []      []      []      []
+[\]     [\]         [\]         [16\10]     []      []      [\]     [\]
+[\]     [3\]        [17\16]     []          []      [6\]    [\]     [\]
+[\23]   []          []          []          []      []      [16\]   [17\]
+[\11]   []          []          [\]         [\19]   []      []      []
+[\]     [\]         [\]         [\]         [\18]   []      []      []
+EOF
+
+puts "After parse"
+$BOARD.prepare()
+puts "After prepare"
+while $BOARD.merge_constraints() or $BOARD.filter_constraints_without_cells()
+    true
+end
+puts "After merge_constraints"
+$BOARD.all_coords.each do |pos|
+    if (pos.x == 0)
+        print "\n"
+    end
+    format = '[%-6s]'
+    c = $BOARD.cell(pos)
+    if c.solid?
+        print sprintf(format, ' \\\\ ')
+    else
+        print sprintf(format, c.get_possible_verdicts.join(','))
+    end
+end
+print "\n"
